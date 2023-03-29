8392817594877e0080edb1570fdb64516c9afdad
BennedictMoraba <mulisa648@gmail.com>
2023-03-29 07:59:04 +0200

    Second Commit

--- ApplicationInstrumentation/myFirstApp.py
+++ ApplicationInstrumentation/myFirstApp.py
@@ -1,13 +1,16 @@
 import http.server
+from prometheus_client import start_http_server
+
 
 class HandleRequests(http.server.BaseHTTPRequestHandler):
     def do_Get(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.writable(bytes("<html><head><title>MyFirst Application</title></head><body><center><h2>Welcome stranger lets get started</h2></center></body></html>","utf-8"))
+        self.wfile.write(bytes("<html><head><title>MyFirst Application</title></head><body style ='color: #333; margin-top: 30px'><center><h2>Welcome stranger lets get started</center></h2></body></html>","utf-8"))
         self.wfile.close
 
         if __name__ == "__main__":
-            server = http.server.HTTPServer(('localhost','5000'), HandleRequests)
+            start_http_server(5001)
+            server = http.server.HTTPServer(('localhost',5000), HandleRequests)
             server.serve_forever()
